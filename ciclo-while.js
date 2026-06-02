ce0f965eb6dd8148fbd687102d1de3bc48ec6068
LilibelFR <figueroa.lilibelus@gmail.com>
2026-06-02 01:34:34 -0500

    +Modificación conteo de propuestas

--- ciclo-while.js
+++ ciclo-while.js
@@ -1,12 +1,18 @@
 let intento = 1
-const maximoIntentos = 3
+let ciclo = 1
+const maximoIntentos = 
+
+if(maximoIntentos < 1)  {
+  console.log("El número máximo de interntos debe ser al menos 1")
+}
 
 console.log("Práctica inicial con while")
 console.log("Vamos a repetir un proceso mientras el intento sea menor o igual al máximo.")
 
 while (intento <= maximoIntentos) {
-  console.log("Intento de revisión número:", intento)
+  console.log("Intento de revisión número:", intento, "Ciclo número:" , ciclo)
   intento = intento + 1
+  ciclo = ciclo + 1
 }
 
 console.log("Proceso de revisión finalizado.")
